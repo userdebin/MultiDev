2646e417be214f32b69707eb248b6cdd88d6797a
Adrocatis <mr.adrocatis@gmail.com>
2024-11-14 05:41:45 +0700

    Update 1.5
    change nama

--- Assets/LobbyTutorial/Scripts/EditPlayerName.cs
+++ Assets/LobbyTutorial/Scripts/EditPlayerName.cs
@@ -17,7 +17,7 @@ public class EditPlayerName : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI playerNameText;
 
 
-    private string playerName = "Code Monkey";
+    private string playerName = "Main Character";
 
 
     private void Awake() {
